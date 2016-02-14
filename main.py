b386136cddf2beef672f43f25460105c78d16140
Kai Duan <duankai@yeah.net>
2016-02-14 09:33:09 +0800

    input some real code

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-# This is the py entry
-# I'll add some code. why can not commit!!!
+import os
+
+def long_fun1_name(
+        name,
+        age):
+    if (name == "kkk"
+            and age > 12):
+        print(name, age)
+    print(name, age)
+
+my_list = [
+    1, 2, 3,
+    4, 5, 6,
+    ]
+
+class Car(object):
+
+
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+    def start()：
+        print("Car is starting")
+
+    def stop():
+        print("Car is stopping")
+
+    def judge():
+        if x == 4:
+            print("Ok")
+        else:
+            print("Refused")
+
+if '__name__==__main__':
+
+    long_fun1_name("kkk", 23)
+    dic = {}
+    dic['first'] = my_list[1]
+    print(dic['first'])
+
+    
